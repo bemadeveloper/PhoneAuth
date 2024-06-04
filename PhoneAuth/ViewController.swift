b96f621c0cc7668b68cbc919e8cc95009e3d4513
bemadeveloper <akbermet1990@icloud.com>
2024-06-04 16:00:14 +0600

    Initial Commit

---
+++ PhoneAuth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneAuth
+//
+//  Created by Bema on 4/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
